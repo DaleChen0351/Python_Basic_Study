329a4eefd8d2b37b12a6131dbae55e828f369e11
DaleChen0351 <dalechen0351@gmail.com>
2019-02-04 17:27:35 +0800

    3-float and integer study

---
+++ corey_schafer_beginner/3-integers_and_floats.py
@@ -0,0 +1,22 @@
+import math
+num =3.14
+print(type(num))
+
+print(3//2) # floor division
+print(3**2)
+print(3%2)# used to know: odd or even
+
+
+num*=10
+print(num)
+print(abs(-5))
+print(round(3.756,2))  #defalut:nearest value, another parameter is used to config how many digits after the point will be left.
+print(math.floor(3.14))
+
+num1=1
+num2=4
+print(num1<=num2) # the return value is bool type
+
+num3="100"
+num4="200"
+print(int(num3)+int(num4)) # casting
